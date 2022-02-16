237f70930f242950c1a9fa52a35dcdc9ff3f6d29
rasz60 <96821067+rasz60@users.noreply.github.com>
2022-02-16 23:34:29 +0900

    Add files via upload

---
+++ spring_framework/jex00/src/main/java/com/wvwv/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.wvwv.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"com.wvwv.sample"})
+public class RootConfig {
+
+}
